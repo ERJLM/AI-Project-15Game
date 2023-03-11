8d16b5802bdfa6ff2bef3339e59f8e3eadfba4ac
FindingBits <36474979+FindingBits@users.noreply.github.com>
2023-03-11 15:37:10 +0000

    small fix

--- Code/SearchAlg.java
+++ Code/SearchAlg.java
@@ -56,6 +56,10 @@ public class SearchAlg{
                     Board Boardi = new Board(a);
                     Board Boardf = new Board(b);
 
+                    if(!Boardi.check() || !Boardf.check()){
+                        throw new IOException("The provided boards are not '15 Game' compliant. Exiting.");
+                    }
+
                     System.out.println("\nStarting the game...");
                     Game game = new Game(Boardi,Boardf);
 
@@ -84,11 +88,11 @@ public class SearchAlg{
                             case "bfs":
                                 game.searchBfs();
                                 break;
-                            case "dls":
+                            case "idfs":
                                 game.searchIdfs();
                                 break;
                             case "help":
-                                System.out.println("dfs - dfs search\nastar - astar search\nastar2 - astar2 search\ngreedy - greedy search\ngreedy2 - greedy2 search\nbfs - bfs search\ndls - dls search");
+                                System.out.println("dfs - dfs search\nastar - astar search\nastar2 - astar2 search\ngreedy - greedy search\ngreedy2 - greedy2 search\nbfs - bfs search\nidfs - idfs search");
                             default:
                                 System.out.println("Invalid game Command!");
                                 break;
