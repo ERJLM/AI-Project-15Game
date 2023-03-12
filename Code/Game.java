0f270fa5e44ba7b9716091c79dc4b9794a5215f9
ERJLM <99908664+ERJLM@users.noreply.github.com>
2023-03-12 13:44:30 +0000

    Update Game.java

--- Code/Game.java
+++ Code/Game.java
@@ -167,7 +167,7 @@ public class Game{
       PriorityQueue<Board> frontier = new PriorityQueue<>(Comparator.comparingInt(n -> n.ManhattanDist(Goal)));
       frontier.add(Board);
       int space = 1; //Number of nodes added to frontier
-      ArrayDeque<String> reached = new ArrayDeque<String>();
+      HashSet<String> reached = new HashSet<String>();
 
       while(!frontier.isEmpty()){
         Board node = frontier.poll();
@@ -210,7 +210,7 @@ public class Game{
         PriorityQueue<Board> frontier = new PriorityQueue<>(Comparator.comparingInt(n -> n.heuristic2(Goal)));
         frontier.add(Board);
         int space = 1; //Number of nodes added to frontier
-        ArrayDeque<String> reached = new ArrayDeque<String>();
+        HashSet<String> reached = new HashSet<String>();
 
         while(!frontier.isEmpty()){
           Board node = frontier.poll();
@@ -256,7 +256,7 @@ public class Game{
              return node;
         }
         ArrayDeque<Board> frontier = new ArrayDeque<Board>();
-        ArrayDeque<String> reached = new ArrayDeque<String>();
+        HashSet<String> reached = new HashSet<String>();
         frontier.add(node);
         int space = 1; //Number of nodes added to frontier
         reached.add(node.toString());
